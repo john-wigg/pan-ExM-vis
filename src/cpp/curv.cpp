4eba48791d25e4d46db03becd1941cd23c256ac9
John Wigg <john.wigg@uni-jena.de>
2021-10-12 18:02:03 +0200

    Switch to lerp for curv generation.

--- src/cpp/curv.cpp
+++ src/cpp/curv.cpp
@@ -15,6 +15,41 @@ val curvature(const val sdf, int width, int height, int depth, float vsx, float 
 #define IDX2(i, j, w) ((i)+(j)*(w))
 #define IDX3(i, j, k, w, h) ((i)+(j)*(w)+(k)*(w)*(h))
 
+inline float sample_sdf_x_off(const std::vector<unsigned char> &sdf, int i, int j, int k, int width, int height, int depth, float h, float hx, float min_sdf, float max_sdf) {
+    float ioff = h / hx;
+    float a = ioff - floor(ioff);
+
+    int i1 = min(max(0, i + (int)floor(ioff)), width-1);
+    int i2 = min(i1 + 1, width-1);
+
+    unsigned char px = (1.0 - a) * sdf[IDX3(i1, j, k, width, height)]  + a * sdf[IDX3(i2, j, k, width, height)];
+    return (float)px / 10.0 - 5.0;
+}
+
+inline float sample_sdf_y_off(const std::vector<unsigned char> &sdf, int i, int j, int k, int width, int height, int depth, float h, float hy, float min_sdf, float max_sdf) {
+    float joff = h / hy;
+    float a = joff - floor(joff);
+
+    int j1 = min(max(0, j + (int)floor(joff)), height-1);
+    int j2 = min(j1 + 1, height-1);
+
+    unsigned char px = (1.0 - a) * sdf[IDX3(i, j1, k, width, height)]  + a * sdf[IDX3(i, j2, k, width, height)];
+    return (float)px / 10.0 - 5.0;
+}
+
+
+inline float sample_sdf_z_off(const std::vector<unsigned char> &sdf, int i, int j, int k, int width, int height, int depth, float h, float hz, float min_sdf, float max_sdf) {
+    float koff = h / hz;
+    float a = koff - floor(koff);
+
+    int k1 = min(max(0, k + (int)floor(koff)), depth-1);
+    int k2 = min(k1 + 1, depth-1);
+
+    unsigned char px = (1.0 - a) * sdf[IDX3(i, j, k1, width, height)]  + a * sdf[IDX3(i, j, k2, width, height)];
+    return (float)px / 10.0 - 5.0;
+}
+
+
 inline float sample_sdf(const std::vector<unsigned char> &sdf, int w, int h, int d, float x, float y, float z, float vsx, float vsy, float vsz, float min_sdf, float max_sdf) {
     // sort of nearest neighbour
     int i = min(max(0, x / vsx), w-1);
@@ -34,17 +69,14 @@ val curvature(const val sdf, int width, int height, int depth, float vsx, float 
             for (int i = 0; i < width; ++i) {
                 int index3 = IDX3(i, j, k, width, height);
 
-                float x = i * vsx;
-                float y = j * vsy;
-                float z = k * vsz;
+                float t1 = sample_sdf_x_off(sdf_data, i, j, k, width, height, depth,  h, vsx, min_sdf, max_sdf);
+                float t2 = sample_sdf_x_off(sdf_data, i, j, k, width, height, depth, -h, vsx, min_sdf, max_sdf);
+                float t3 = sample_sdf_y_off(sdf_data, i, j, k, width, height, depth,  h, vsy, min_sdf, max_sdf);
+                float t4 = sample_sdf_y_off(sdf_data, i, j, k, width, height, depth, -h, vsy, min_sdf, max_sdf);
+                float t5 = sample_sdf_z_off(sdf_data, i, j, k, width, height, depth,  h, vsz, min_sdf, max_sdf);
+                float t6 = sample_sdf_z_off(sdf_data, i, j, k, width, height, depth, -h, vsz, min_sdf, max_sdf);
 
-                float t1 = sample_sdf(sdf_data, width, height, depth, x + h, y    , z    , vsx, vsy, vsz, min_sdf, max_sdf);
-                float t2 = sample_sdf(sdf_data, width, height, depth, x - h, y    , z    , vsx, vsy, vsz, min_sdf, max_sdf);
-                float t3 = sample_sdf(sdf_data, width, height, depth, x    , y + h, z    , vsx, vsy, vsz, min_sdf, max_sdf);
-                float t4 = sample_sdf(sdf_data, width, height, depth, x    , y - h, z    , vsx, vsy, vsz, min_sdf, max_sdf);
-                float t5 = sample_sdf(sdf_data, width, height, depth, x    , y    , z + h, vsx, vsy, vsz, min_sdf, max_sdf);
-                float t6 = sample_sdf(sdf_data, width, height, depth, x    , y    , z - h, vsx, vsy, vsz, min_sdf, max_sdf);
-                float t  = sample_sdf(sdf_data, width, height, depth, x    , y    , z    , vsx, vsy, vsz, min_sdf, max_sdf);
+                float t  = sample_sdf_x_off(sdf_data, i, j, k, width, height, depth,  0, vsx, min_sdf, max_sdf);
                 
                 float c = (t1+t2+t3+t4+t5+t6-6.0*t)/(h*h);
                 
